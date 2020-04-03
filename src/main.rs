25d6043614bc091a8f96a47de7218ba1a3cc045e
0x20F <alexhexan22@gmail.com>
2020-04-03 09:30:15 +0200

    Testing how config gets parsed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use git2::Repository;
 use clap::{ App, load_yaml };
 use paris::{ log };
+use serde::{ Deserialize };
+use std::fs::read_to_string;
+use indexmap::IndexMap;
+
+
+
+
+#[derive(Deserialize)]
+struct Config {
+    test: String,
+
+    packages: IndexMap<String, Package>
+}
+
+#[derive(Debug, Deserialize)]
+struct Package {
+    name: String
+}
 
 
 
@@ -10,11 +28,17 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from(yaml).get_matches();
 
+    let file = read_to_string("packages.toml").unwrap();
+    let config: Config = toml::from_str(&file).unwrap();
+
+    println!("{:?}", config.test);
+    println!("{:?}", config.packages);
 
     if let Some(cmd) = matches.subcommand_name() {
         match cmd {
             "test" => {
-                log!("<bright green>Status<//>: you just ran the <u>test</u> command");
+                log!("<bright blue>Info</>: directory this was run in: {}", std::env::current_dir().unwrap().display());
+                log!("<bright green>Status</>: you just ran the <u>test</u> command");
             }
             _ => unreachable!()
         }
