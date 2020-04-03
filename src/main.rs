22920105b6dd2f7f8048d4d75e951112fa4df536
0x20F <alexhexan22@gmail.com>
2020-04-03 07:42:19 +0200

    Testing clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use git2::Repository;
+use clap::{ App, load_yaml };
+
 
 
 
 fn main() {
-    let url = "https://github.com/0x20F/logger";
 
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+
+    if let Some(cmd) = matches.subcommand_name() {
+        match cmd {
+            "test" => println!("You just ran the test command!"),
+            _ => println!("Whatever you just ran was wrong!")
+        }
+    };
+
+    /*let url = "https://github.com/0x20F/logger";
     match Repository::clone(url, "repositories/") {
         Ok(repo) => repo,
         Err(e) => panic!("Failed to clone repo: {}", e)
-    };
+    };*/
 }
