1e4dc6ca5a5f7a1aef40e22c08e354edf13a995b
Poly <alexhexan22@gmail.com>
2020-04-04 12:25:58 +0200

    Initialize the app normally

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod config;
 
 
 use git2::Repository;
-use clap::{ App, load_yaml };
+use clap::{App, SubCommand, Arg};
 use paris::{ log };
 use std::fs::read_to_string;
 
@@ -15,24 +15,33 @@ use config::Config;
 
 fn main() {
 
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from(yaml).get_matches();
+    let matches = App::new("smoke")
+        .subcommand(
+            App::new("cover")
+                .about("Starts downloading everything")
+                .arg(
+                    Arg::with_name("config")
+                        .long("config")
+                        .takes_value(true)
+                        .value_name("FILE")
+                        .help("Path to the config file")
+                )
+        ).get_matches();
 
-    let file = read_to_string("packages.toml").unwrap();
-    let config: Config = toml::from_str(&file).unwrap();
 
-    if let Some(cmd) = matches.subcommand_name() {
-        match cmd {
-            "cover" => {
-                for (_, package) in config.packages.into_iter() {
-                    println!("Found package: {}", package.name);
-                }
 
-                log!("<bright blue>Info</>: directory this was run in: {}", std::env::current_dir().unwrap().display());
-                log!("<bright green>Status</>: you just ran the <u>test</u> command");
-            }
-            _ => unreachable!()
-        }
+    if let Some(ref matches) = matches.subcommand_matches("cover") {
+        println!("{:?}", matches.value_of("config"));
+
+        /*let file = read_to_string("packages.toml").unwrap();
+        let config: Config = toml::from_str(&file).unwrap();
+
+        for (_, package) in config.packages.into_iter() {
+            println!("Found package: {}", package.name);
+        }*/
+
+        log!("<bright blue>Info</>: directory this was run in: {}", std::env::current_dir().unwrap().display());
+        log!("<bright green>Status</>: you just ran the <u>test</u> command");
     };
 
     /*let url = "https://github.com/0x20F/logger";
