1a98e5aed0ba095a3071e24d174cd406961b4efb
Poly <alexhexan22@gmail.com>
2020-04-09 22:04:23 +0200

    Remove unused trait

--- src/config/script.rs
+++ src/config/script.rs
@@ -1,17 +1,7 @@
 use serde::Deserialize;
-use std::str::FromStr;
 
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct Script {
     script: String
-}
-
-
-impl FromStr for Script {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self { script: s.to_owned() })
-    }
 }
