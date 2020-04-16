b5e8a99c7612dc75ea76bd999b55f8ae6c720b3a
0x20F <alexhexan22@gmail.com>
2020-04-16 11:36:50 +0200

    Show message when cloning as well

--- src/download/git.rs
+++ src/download/git.rs
@@ -2,21 +2,20 @@ use crate::error::Error;
 
 use git2::Repository;
 use std::path::Path;
-
-
+use crate::output::{Out, Type};
 
 
 pub fn clone(url: &str, out: &str) -> Result<(), Error> {
-    println!("\tCloning repo");
-
     match Repository::clone(&url, &out) {
         Ok(_) => (),
         Err(_) => {
-            if !Path::new(&out).exists() {
+            if !Path::new(out).exists() {
                 return Err(Error::Clone);
             }
         }
     };
+
+    Out::write(Type::Done, &format!("downloading {}", url));
 
     Ok(())
 }
